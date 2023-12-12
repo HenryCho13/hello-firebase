bf652c500cdc3174c83d00296d01a5917b8f06a7
Henry Cho <hcho2396@gmail.com>
2023-12-11 23:08:36 -0500

    add ant components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { Button, Space, DatePicker, version, ColorPicker } from 'antd';
 
-function App() {
-  return (
-    <h1>Hello Firebase! This is awesome!</h1>
-  );
-}
+const App = () => (
+  <div style={{ padding: '0 24px' }}>
+      <h1>First Firebase/React Web App{version}</h1>
+    <Space>
+      <DatePicker />
+      <Button type="primary">Click</Button>
+      <ColorPicker/>
+    </Space>
+  </div>
+);
 
 export default App;
